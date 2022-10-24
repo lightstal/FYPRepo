d8c723fb537fff3d9d43a3b74717001d0ed95acd
lightstal <50443758+lightstal@users.noreply.github.com>
2022-10-24 21:10:38 +0800

    Completed Data Ingestion
    To do : implement NMS List into forloop

--- FYP_Project/SplunkScripts/DataIngestion/IngestingData.js
+++ FYP_Project/SplunkScripts/DataIngestion/IngestingData.js
@@ -34,7 +34,7 @@ function uploadDataToSplunk(data){
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Submitted job");
+                    console.log("Submitted job with SID: " + job);
                 }
             }
             )
@@ -46,37 +46,45 @@ function uploadDataToSplunk(data){
 async function main(selectedOrgs, mainOrganization) {
     let deviceDataList = []
     await deviceData(selectedOrgs, mainOrganization).then(async (deviceData) => {
-    // console.log(deviceData)
-        for (let i = 0; i < deviceData.length; i++) {
-            for (let j = 0; j < deviceData[i].length; j++) {
-                try{
-                    // change lastContact to GMT+8 format from UNIX timestamp
-                    let lastContact = new Date(deviceData[i][j].lastContact * 1000)
-                    lastContact.setHours(lastContact.getHours() + 8)
-                    deviceData[i][j].lastContact = lastContact.toISOString()
-                }
-                catch(err){
-                    console.log(err)
-                }
-                try{
-                    let createdTIme = new Date(deviceData[i][j].created * 1000)
-                    createdTIme.setHours(createdTIme.getHours() + 8)
-                    deviceData[i][j].created = createdTIme.toISOString()
-                }
-                catch(err){
-                    console.log(err)
-                }
-                // console.log(deviceData[i][j])
-                deviceDataList.push(deviceData[i][j])
+            // console.log(deviceData)
+            for (let i = 0; i < deviceData.length; i++) {
+                for (let j = 0; j < deviceData[i].length; j++) {
+                    try {
+                        // change lastContact to GMT+8 format from UNIX timestamp
+                        let lastContact = new Date(deviceData[i][j].lastContact * 1000)
+                        lastContact.setHours(lastContact.getHours() + 8)
+                        deviceData[i][j].lastContact = lastContact.toISOString()
+                    } catch (err) {
+                        console.log(err)
+                    }
+                    try {
+                        let createdTIme = new Date(deviceData[i][j].created * 1000)
+                        createdTIme.setHours(createdTIme.getHours() + 8)
+                        deviceData[i][j].created = createdTIme.toISOString()
+                    } catch (err) {
+                        console.log(err)
+                    }
+                    // console.log(deviceData[i][j])
+                    deviceDataList.push(deviceData[i][j])
 
+                }
             }
         }
-    }
+    )
+    await softwareData(deviceDataList, mainOrganization).then((softwareData) => {
+            // Check if deviceDataList id matches softwareData id
+            for (let i = 0; i < deviceDataList.length; i++) {
+                if(softwareData.deviceId === deviceDataList[i].id){
+                    deviceDataList[i].software = softwareData
+                }
+            }
+        }
     )
     return deviceDataList
 }
 
-main([12,13], "Internal").then((deviceDataList) => {
+
+main([1,2,12,13], "Internal").then((deviceDataList) => {
     uploadDataToSplunk(deviceDataList)
 }
 ).catch((err) => {
